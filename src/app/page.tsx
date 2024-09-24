450c0ee65218810a3850f2ebd4e99ba223909617
Shuffled720 <shuffled720@gmail.com>
2024-09-24 23:58:22 +0530

    dashboard skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,42 @@
-"use client";
-
-import Cards from "@/components/HomePage/Cards";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-import tournament from "../data/tournament.json";
-import axios from "axios";
-import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { cookies } from 'next/headers'; // To access cookies
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 
 export default function Home() {
-  const router = useRouter();
-  const tournamentdata = tournament.tournamentdata;
-  const handleLogout = async () => {
-    try {
-      // Call the logout API
-      await axios.post('/api/userlogout');
-      // Redirect to login page after successful logout
-      router.push('/login');
-    } catch (error) {
-      console.error('Logout failed', error);
-    }
-  };
+  const cookieStore = cookies();
+  const token = cookieStore.get('token')?.value;
   return (
     <>
-      {/* <Link href={"/registration"}>Registration</Link>
-      <Link href={"/login"}>Login</Link> */}
+      <nav className="shadow-lg grid grid-cols-2 justify-items-center">
 
-      <>
+        <div className="">
+          <Image width={100} height={100} src="/images/logo.png" alt="PULSE" />
+        </div>
+      </nav>
 
-        {/* <nav className="bg-green-900 rounded-b-lg grid grid-cols-2 justify-items-center"> */}
-        <nav className="backdrop-blur-sm rounded-b-lg grid grid-cols-2 justify-items-center">
-
-          <div className="">
-            <Image width={100} height={100} src="/images/logo.png" alt="PULSE" />
-          </div>
-          <div className="grid gird-cols-2  flex items-center">
-            <Link href="/registration">
-              <Button >Registration</Button>
-            </Link>
-            <Link href="/login">
-              <Button className="w-full" >Login</Button>
-            </Link>
-            <Button onClick={handleLogout}>Logout</Button>
-          </div>
-          <div>
-
-          </div>
-        </nav>
-        <p className="text-center	text-white text-5xl p-5">Select Tournament</p>
-        <p className="text-center	text-white text-3xl">Total <span className="text-yellow-300">3+</span> ongoing tournament registration!!!</p>
-        <ul className="demos">
-          {tournamentdata.map((data, index) => {
-            return (<>
-              {data ? <><li key={index}><Cards data={data} /></li></> : <></>}
-            </>)
-          })}
-        </ul>
-
-
-      </>
+      <div className='m-10'>
+        <h1 className='text-white text-center text-white text-5xl p-5 font-sans'>Welcome to Webbuzz</h1>
+        <p className='text-white text-center'>Lorem ipsum dolo Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque sed ipsa corrupti minima eius dolores distinctio! Pariatur voluptatem eligendi nam fugiat, nemo ipsum fuga nostrum, vitae magnam tenetur saepe amet.r sit amet consectetur adipisicing elit. Rem dolores molestias, recusandae veritatis corrupti fugiat. Illum fugiat ut veritatis, odit perferendis architecto impedit similique magni quia aliquam. Sed, voluptatibus necessitatibus.</p>
+      </div>
+      {token ?
+        <>
+          <Button asChild>
+            <Link href="/dashboard">Continue</Link>
+          </Button>
+        </>
+        :
+        <>
+          <Button asChild>
+            <Link href="/login">Login</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/register">Register</Link>
+          </Button>
+        </>}
 
     </>
+
   );
 }
