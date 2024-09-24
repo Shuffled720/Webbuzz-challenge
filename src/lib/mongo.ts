0263ed9a25260bb8f29c3eed389ea0c88aa0d332
Shuffled720 <shuffled720@gmail.com>
2024-09-24 21:37:49 +0530

    DB SORTED

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -3,39 +3,11 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || '';
 
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
-}
-
-/**
- * Global is used to maintain a cached connection across hot reloads in development.
- * This prevents connections from growing exponentially during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn;
+const connectToDatabase= async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
+  return mongoose.connect(MONGODB_URI);
+};
 export default connectToDatabase;
